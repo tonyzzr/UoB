9f241e7e3c12af02ce2dd04b6f8124216be33f97
Zhuorui Zhang <tonyzzr@users.noreply.github.com>
2023-11-19 12:40:43 -0500

    Create image_fusion.py

---
+++ model/image_fusion.py
@@ -0,0 +1,19 @@
+import cv2
+
+def cv2rigid_transform(imgs, matrices3d):
+  transformed_imgs = torch.zeros_like(imgs)
+
+  rc_ind = [0, 1, 3]
+  matrices2d = matrices3d[:, rc_ind, :][:, :, rc_ind]
+  matrices2d = matrices2d[:, 0:2, :]
+
+  for i in range(imgs.shape[0]):
+    img = imgs[i, 0, ...].numpy()
+    mat = matrices2d[i, :, :].numpy()
+
+    # print(mat.shape)
+
+    img = cv2.warpAffine(img, mat, (img.shape[1], img.shape[0]))
+    transformed_imgs[i, 0, ...] = torch.tensor(img)
+
+  return transformed_imgs
