9a6b112febc144f77d5d116f0ddaadeaaa22e6ad
Zhuorui Zhang <tonyzzr@users.noreply.github.com>
2023-11-19 12:36:01 -0500

    organized the code

--- model/rigid_link.py
+++ model/rigid_link.py
@@ -28,10 +28,10 @@ class RigidLink:
     x1, y1 = x0 + self.length, y0
     link_x = torch.linspace(x0, x1, self.n_link_points)
     link_y = torch.linspace(y0, y1, self.n_link_points)
-    self.link_coordinates = torch.stack([link_x, link_y, 
-                             torch.zeros_like(link_x), 
+    self.link_coordinates = torch.stack([link_x, link_y,
+                             torch.zeros_like(link_x),
                              torch.ones_like(link_y)], dim=1).T
-    
+
 
 
   def set_thetas(self, angles):
@@ -39,6 +39,31 @@ class RigidLink:
     # should i accumulate the angles?
     self.thetas = thetas
     return self.thetas
+
+  def calc_rotation_pose(self, theta, center:tuple):
+    '''
+      Lie tensor of rotation in SE3:
+        - theta: angle of rotation
+        - center: (x0, y0) center of rotation
+    '''
+    x0, y0 = center
+    
+    theta_vec = torch.tensor([0, 0, 1]) * theta
+    R_SO3 = pp.so3(theta_vec).Exp()
+    R_SE3 = torch.cat([torch.tensor([0, 0, 0,]), R_SO3.tensor()])
+
+    M = []
+    M.append(pp.SE3([-x0, -y0, 0, 0, 0, 0, 1]))
+    M.append(pp.SE3(R_SE3))
+    M.append(pp.SE3([x0, y0, 0, 0, 0, 0, 1]))
+
+    transform_mat = pp.identity_SE3(1).matrix()
+    for m in M:
+      transform_mat = m.matrix() @ transform_mat
+    rotation_pose = pp.mat2SE3(transform_mat)
+
+    return rotation_pose
+
 
   def calc_rela_poses(self,):
     '''
@@ -49,29 +74,22 @@ class RigidLink:
         2. Rotate around (x0 + l, y0)
       ---
       This is also equal to:
-        1. Rotate around (x0, y0)    
+        1. Rotate around (x0, y0)
         2. Move (x0, y0) to (x0 + l, y0)
     '''
     for i in range(1, self.n):
       M = [] # a sequence of transformations applied to derive relative pose
       theta = self.thetas[i]
 
-      # construct rotation in SE3
-      theta_vec = torch.tensor([0, 0, 1]) * theta
-      R_SO3 = pp.so3(theta_vec).Exp()
-      R_SE3 = torch.cat([torch.tensor([0, 0, 0,]), R_SO3.tensor()])
-
       # move (0, 0) to (L, 0) == Move (x0, y0) to (x0 + l, y0)
       M.append(pp.SE3([self.length, 0, 0, 0, 0, 0, 1]))
 
-      # rotate around (x0 + l, y0) == (x1, y1)
-      x0, y0 = self.origin[0], self.origin[1] 
+      # # rotate around (x0 + l, y0) == (x1, y1)
+      x0, y0 = self.origin[0], self.origin[1]
       x1, y1 = x0 + self.length, y0
+      M.append(self.calc_rotation_pose(theta=theta, center=(x1, y1)))
 
-      M.append(pp.SE3([-x1, -y1, 0, 0, 0, 0, 1]))
-      M.append(pp.SE3(R_SE3))
-      M.append(pp.SE3([x1, y1, 0, 0, 0, 0, 1]))
-
+      # compose all transformations
       transform_mat = self.rela_poses[i].matrix()
       for m in M:
         transform_mat = m.matrix() @ transform_mat
@@ -80,35 +98,25 @@ class RigidLink:
     return self.rela_poses
 
   def calc_global_poses(self,):
-    global_poses = self.global_poses
 
-    # # global pose of the first array
-    # # 1 - rotate around (0, 0) of theta0
-    # theta0 = self.thetas[0]
-    # theta_vec = torch.tensor([0, 0, 1]) * theta0
-    # R_SO3 = pp.so3(theta_vec).Exp()
-    # R_SE3 = torch.cat([torch.tensor([0, 0, 0,]), R_SO3.tensor()])
+    # 1 - global pose of the first array #
+    # we can't let it rotate theta[0] around (0, 0), as (x0, y0) may be transformed to minus values
+    # we should let it rotate around (x0, y0)
+    M = []
+    x0, y0 = self.origin[0], self.origin[1]
+    self.global_poses[0] = self.calc_rotation_pose(theta=self.thetas[0], center=(x0, y0))
 
-    # # 2 - move the origin to self.origin (x0, y0)
-    # x0, y0 = self.origin[0], self.origin[1]
-    # T_SE3 = pp.SE3([x0, y0, 0, 0, 0, 0, 1])
-
-    # transform_mat = T_SE3.matrix() @ pp.SE3(R_SE3).matrix()
-    # global_poses[0] = pp.mat2SE3(transform_mat)
-
-
-    # global poses of all other arrays
+    # 2 - global poses of all other arrays #
     for i in range(1, self.n):
-      transform_mat =  global_poses[i-1].matrix() @ self.rela_poses[i].matrix() # do relative transformation first
-      global_poses[i] = pp.mat2SE3(transform_mat)
+      transform_mat =  self.global_poses[i-1].matrix() @ self.rela_poses[i].matrix() # do relative transformation first
+      self.global_poses[i] = pp.mat2SE3(transform_mat)
 
-    self.global_poses = global_poses
     return self.global_poses
 
 
   def calc_joint_locations(self,):
     # first joint locates (x1, y1)
-    x0, y0 = self.origin[0], self.origin[1] 
+    x0, y0 = self.origin[0], self.origin[1]
     x1, y1 = x0 + self.length, y0
 
     joint_homo_coord = torch.tensor([x1, y1, 0, 1], dtype=torch.float32)
@@ -157,16 +165,16 @@ class RigidLink:
       fig, ax = plt.subplots(1, 1, figsize=(6, 3))
 
       # plot original elements
-      ax.scatter(self.link_coordinates[0, ...], 
-                 self.link_coordinates[1, ...], 
+      ax.scatter(self.link_coordinates[0, ...],
+                 self.link_coordinates[1, ...],
                  marker='x', c='k', s=10)
-      
+
       # highlight the first element in the transformed array
-      ax.scatter(transdformed_link_coordinates[i, 0, 0], 
+      ax.scatter(transdformed_link_coordinates[i, 0, 0],
                  transdformed_link_coordinates[i, 1, 0], c='b', s=10)
-      
+
       # plot all elements in the transformed array
-      ax.scatter(transdformed_link_coordinates[i, 0, ...], 
+      ax.scatter(transdformed_link_coordinates[i, 0, ...],
                  transdformed_link_coordinates[i, 1, ...], c='r', s=1)
 
       ax.axis('equal')
@@ -179,20 +187,20 @@ class RigidLink:
 
     if ax is None:
       fig, ax = plt.subplots(1, 1, figsize=(6, 6))
-    
+
     # plot original elements
-    ax.scatter(self.link_coordinates[0, ...], 
-               self.link_coordinates[1, ...], 
+    ax.scatter(self.link_coordinates[0, ...],
+               self.link_coordinates[1, ...],
                marker='x', c='k', s=10)
-    
+
     for i in range(self.n):
 
       # highlight the first element in the transformed array
-      ax.scatter(transdformed_link_coordinates[i, 0, 0], 
+      ax.scatter(transdformed_link_coordinates[i, 0, 0],
                  transdformed_link_coordinates[i, 1, 0], c='b', s=10)
-      
+
       # plot all elements in the transformed array
-      ax.scatter(transdformed_link_coordinates[i, 0, ...], 
+      ax.scatter(transdformed_link_coordinates[i, 0, ...],
                  transdformed_link_coordinates[i, 1, ...], c='r', s=1)
 
     ax.axis('equal')
