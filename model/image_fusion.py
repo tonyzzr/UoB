c44e4b0cd2de717acde2a730ad47ab5c8d5c2c14
Zhuorui Zhang <tonyzzr@users.noreply.github.com>
2023-11-25 02:53:37 -0500

    Update image_fusion.py

--- model/image_fusion.py
+++ model/image_fusion.py
@@ -1,1 +1,12 @@
+import torch
 
+def mean_image_fusion(registered_imgs):
+  n_view = registered_imgs.shape[0]
+  fused_img = torch.zeros_like(registered_imgs[0, 0, ...])
+
+  for i in range(n_view):
+    fused_img += registered_imgs[i, 0, ...]
+
+  fused_img /= n_view
+
+  return fused_img
