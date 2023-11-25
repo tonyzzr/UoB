04361aacd5c93d40f05396df8d780f108367b641
Zhuorui Zhang <tonyzzr@users.noreply.github.com>
2023-11-25 03:12:31 -0500

    Update apply_pose.py

--- model/apply_pose.py
+++ model/apply_pose.py
@@ -2,19 +2,22 @@ import cv2
 import torch
 
 def cv2apply_poses(imgs, matrices3d):
+  assert len(imgs.shape) == 3, "img dimension should be (N, H, W)"
+  
   transformed_imgs = torch.zeros_like(imgs)
 
   rc_ind = [0, 1, 3]
   matrices2d = matrices3d[:, rc_ind, :][:, :, rc_ind]
   matrices2d = matrices2d[:, 0:2, :]
 
-  for i in range(imgs.shape[0]):
-    img = imgs[i, 0, ...].numpy()
-    mat = matrices2d[i, :, :].numpy()
+  n_view = imgs.shape[0]
+  for i in range(n_view):
+    img = imgs[i, ...].numpy()
+    mat = matrices2d[i, ...].numpy()
 
     # print(mat.shape)
 
     img = cv2.warpAffine(img, mat, (img.shape[1], img.shape[0]))
-    transformed_imgs[i, 0, ...] = torch.tensor(img)
+    transformed_imgs[i, ...] = torch.tensor(img)
 
   return transformed_imgs
