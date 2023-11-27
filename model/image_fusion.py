ee1955e3b3646d334e4560026139fb204d5f65fe
Zhuorui Zhang <tonyzzr@users.noreply.github.com>
2023-11-27 07:07:09 -0500

    Update image_fusion.py

--- model/image_fusion.py
+++ model/image_fusion.py
@@ -12,3 +12,24 @@ def mean_image_fusion(registered_imgs):
   fused_img /= n_view
 
   return fused_img
+
+#@title Fusers
+def max_fuser(registered_imgs, **kwargs):
+  assert type(registered_imgs) == torch.Tensor
+  assert registered_imgs.ndim == 3
+  
+  return torch.max(registered_imgs, dim=0)
+
+def weighted_mean_fuser(registered_imgs, registered_masks, eps=0.5, **kwargs):
+  assert type(registered_imgs) == torch.Tensor
+  assert registered_imgs.ndim == 3
+  assert type(registered_masks) == torch.Tensor
+  assert registered_masks.ndim == 3
+  
+  return torch.sum(registered_imgs * registered_masks, dim=0) / (torch.sum(registered_masks, dim=0) + eps)
+
+def mean_fuser(registered_imgs, **kwargs):
+  assert type(registered_imgs) == torch.Tensor
+  assert registered_imgs.ndim == 3
+
+  return torch.mean(registered_imgs, dim=0)
